66eafa54434179fdf507dba1f1d4690bdcccdcac
bluesilver4 <32588966+bluesilver4@users.noreply.github.com>
2017-10-07 17:33:23 +1030

    Add files via upload

--- main_test.cpp
+++ main_test.cpp
@@ -16,13 +16,14 @@ int money(cost *object)
 }
 int main()
 {
-	company eco;
+	/*company eco;
 
 	//naming company and outputting name
 	eco.change_company_name();
 	cout<<eco.get_name()<<endl;
 
-	building junk, Yard;
+	building junk(3, 2, "stone", 3, "Paris");
+	building Yard(2, 1, "paper", 0, "Paris-street");
 
 	//cout<<junk.get_ID()<<endl;
 	//cout<<Yard.get_ID()<<endl;
@@ -32,15 +33,36 @@ int main()
 
 	building* tracker = eco.get_buildings();
 	cout<<"ID of building"<<endl;
+	cout<<money(&junk)<<endl;
+
 	for(int i = 0 ; i < eco.get_no_buildings(); i++)
 	{
 		cout << (tracker[i].get_ID())<<endl;
 	}
 
-	cout<<money(&junk)<<endl;
+	
+
+	tracker = eco.remove_building(0);
+
+	for(int i = 0 ; i < eco.get_no_buildings(); i++)
+	{
+		cout << (tracker[i].get_ID())<<endl;
+	}*/
 
 
+	company eco;
+	eco.change_company_name("eco");
+	cout<<eco.get_name()<<endl;
+	int i =0;
+	while(i ==0)
+	{
+		building junk(3, 2, "stone", 3, "Paris");
+		cout<<"enter i"<<endl;
+		cin >> i;
+		cout<<junk.get_ID()<<endl;
+	}
 
+	
 
 
 }
