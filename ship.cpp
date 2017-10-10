150921df507245d4325c05451f285b7f33874014
nedlio <32534738+nedlio@users.noreply.github.com>
2017-10-10 06:07:54 +0000

    Update ship.cpp

--- ship.cpp
+++ ship.cpp
@@ -11,15 +11,15 @@ ship::ship()
 	ship_name = "Vacant";
 }
 
-ship::ship(int initial_crew_capacity, string inital_ship_name)
+ship::ship(int initial_crew_capacity, string inital_ship_name)//takes crew capacity and name from the user function
 {
 	shipID = currentID;
-	currentID++;
+	currentID++; //increments currentID
 	crew_capacity = initial_crew_capacity;
 	ship_name = inital_ship_name;
 }
 
-void ship::crew_quaters_renevation(int new_crew_capacity)
+void ship::crew_quaters_renevation(int new_crew_capacity)//add new crew capacity from the user function
 {
 	crew_capacity = new_crew_capacity;
 }
@@ -29,22 +29,22 @@ int ship::get_crew_amount()
 	return crew_capacity;
 }
 
-string ship::get_ship_name()
+string ship::get_ship_name()//returns name from user function
 {
 	return ship_name;
 }
 
-void ship::change_ship_name(string new_name)
+void ship::change_ship_name(string new_name)//change name to boatymcboatface
 {
 	ship_name = new_name;
 }
 
-int ship::get_ship_id()
+int ship::get_ship_id()//returns shipID from user function
 {
 	return shipID;
 }
 
-int ship::get_maintance_cost()
+int ship::get_maintance_cost()//creates a lit maintenance cost 
 {
 	return crew_capacity * 5;
 }
