1f593538ccd51ad3b53a21bada07f8e3971e5bb5
nedlio <32534738+nedlio@users.noreply.github.com>
2017-10-12 15:05:07 +1030

    Update carrier.cpp

--- carrier.cpp
+++ carrier.cpp
@@ -4,26 +4,31 @@
 #include <string>
 using namespace std;
 
+//default carrier ship initialiser
 carrier::carrier() : ship()
 {
 	storage_capacity =0;
 }
 
+//takes in storage capacity, crew capacity and ship name from the main function and implements it
 carrier::carrier(int initial_storage_capacity,int initial_crew_capacity, string inital_ship_name) : ship(initial_crew_capacity, inital_ship_name)
 {
 	storage_capacity = initial_storage_capacity;
 }
 
+//returns the storage capacity
 int carrier::get_storage_capacity()
 {
 	return storage_capacity;
 }
 
+//allows the storage capacity to be modified
 void carrier::ship_capactiy_renevation(int new_stroage_capacity)
 {
 	storage_capacity = new_stroage_capacity;
 }
 
+//calculates the maintenance cost and returns it
 int carrier::get_maintance_cost()
 {
 	int cost = storage_capacity*5 + crew_capacity*10;
