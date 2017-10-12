426189c4a5bac9923603bed589e249b9b1e8a95e
PatrickJ1 <32534470+PatrickJ1@users.noreply.github.com>
2017-10-12 17:17:28 +1030

    commenting

--- building.h
+++ building.h
@@ -4,18 +4,19 @@
 #include <string>
 #include "cost.h"
 
-class building : public cost
+//abstract class for the different types of buildings; office & warehouse
+class building : public cost //inherits the public function from cost.h
 {
-	
+	//initializing variables 
 	std::string building_material;
 	std::string branch_location;
 	int buildingID;
 	static int currentID;
 
 protected:
-
 	int worker_capacity;
 	
+	//initializing functions
 public:
 	building();
 	building(std::string inital_building_material, 
