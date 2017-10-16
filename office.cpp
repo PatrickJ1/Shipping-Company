538156ac0976ee1147ab84f6e6d79f32eb3a2b80
PatrickJ1 <32534470+PatrickJ1@users.noreply.github.com>
2017-10-16 23:28:57 +1030

    Update office.cpp

--- office.cpp
+++ office.cpp
@@ -4,12 +4,17 @@
 #include <string>
 using namespace std;
 
+/*default constructor, takes in functions and variables from
+the building abstract class */
 office::office() : building()
 {
 	no_of_office_spaces = 0;
 	no_of_meeting_rooms =0;
 }
 
+/* Takes in offices space, number of meeting rooms,
+building material, worker capacity and branch location
+from main function and implements into functions*/
 office::office(int inital_no_of_office_spaces, 
 		       int inital_no_of_meeting_rooms, 
 		       string inital_building_material, 
@@ -23,6 +28,7 @@ office::office(int inital_no_of_office_spaces,
 	no_of_meeting_rooms = inital_no_of_meeting_rooms;
 }
 
+//allows for the office space to be changed retroactively
 void office::renevate_office_spaces(int new_no_of_office_spaces)
 {
 	no_of_office_spaces = new_no_of_office_spaces;
@@ -33,6 +39,7 @@ int office::get_no_of_office_spaces()
 	return no_of_office_spaces;
 }
 
+//allows for the office space to be changed retroactively
 void office::renevate_meeting_rooms(int new_no_of_meeting_rooms)
 {
 	no_of_meeting_rooms = new_no_of_meeting_rooms;
@@ -48,6 +55,7 @@ string office::get_building_type()
 	return "office";
 }
 
+//calculates maintenance cost
 int office::get_maintance_cost()
 {
 	int cost = no_of_office_spaces * 5 + no_of_meeting_rooms*4
