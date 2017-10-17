523e7daca3871dc29337d959880cecc28bd66402
nedlio <32534738+nedlio@users.noreply.github.com>
2017-10-17 14:06:17 +1030

    Update carrier.h

--- carrier.h
+++ carrier.h
@@ -2,10 +2,12 @@
 #define CARRIER_H
 #include "ship.h"
 
+//inherits from the ship class
 class carrier : public ship
 {
 private:
 	int storage_capacity; // 1 is equivalent to 1 container
+//initializing functions
 public:
 	carrier();
 	carrier(int initial_storage_capacity,int initial_crew_capacity, std::string inital_ship_name);
