20a487e34b54d367bac59aa5505edd3d9cb948e1
nedlio <32534738+nedlio@users.noreply.github.com>
2017-10-17 13:10:26 +1030

    Update ship.cpp

--- ship.cpp
+++ ship.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 int ship::currentID = 0;
 
+/*default constructor, takes in functions and variables from
+the ship abstract class */
 ship::ship()
 {
 	
@@ -11,6 +13,8 @@ ship::ship()
 	ship_name = "Vacant";
 }
 
+/* Takes in crew capacity and ship name from main function and 
+implements into functions*/
 ship::ship(int initial_crew_capacity, string inital_ship_name)
 {
 	shipID = currentID;
@@ -19,31 +23,37 @@ ship::ship(int initial_crew_capacity, string inital_ship_name)
 	ship_name = inital_ship_name;
 }
 
+//allows crew capacity to be changed retroactively
 void ship::crew_quaters_renevation(int new_crew_capacity)
 {
 	crew_capacity = new_crew_capacity;
 }
 
+//returns crew capacity
 int ship::get_crew_amount()
 {
 	return crew_capacity;
 }
 
+// returns ship name
 string ship::get_ship_name()
 {
 	return ship_name;
 }
 
+//allows ship name to be changed retroactively
 void ship::change_ship_name(string new_name)
 {
 	ship_name = new_name;
 }
 
+//returns ship id
 int ship::get_ship_id()
 {
 	return shipID;
 }
 
+//calculates maintenance cost
 int ship::get_maintance_cost()
 {
 	return crew_capacity * 5;
