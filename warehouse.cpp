2079ab92e6f77329dbc01a1a410f89c1613b0592
nedlio <32534738+nedlio@users.noreply.github.com>
2017-10-17 13:34:32 +1030

    Update warehouse.cpp

--- warehouse.cpp
+++ warehouse.cpp
@@ -4,6 +4,9 @@
 #include <string>
 using namespace std;
 
+
+/*default constructor, takes in functions and variables from
+the building abstract class */
 warehouse::warehouse() : building()
 {
 	no_ship_repair_stations = 0;
@@ -11,6 +14,9 @@ warehouse::warehouse() : building()
 	no_ports = 0;
 }
 
+/* Takes in building material, worker capacity, branch location, 
+#of shipRepair stations, #of truck loading bays and #of ports
+from main function and implements into functions*/
 warehouse::warehouse(string inital_building_material, 
 		      		 int inital_worker_capacity, 
 		      		 string inital_branch_location, 
@@ -27,36 +33,43 @@ warehouse::warehouse(string inital_building_material,
 	no_ports = inital_no_ports;
 }
 
+//returns #of ship repair stations
 int warehouse::get_no_ship_repair_stations()
 {
 	return no_ship_repair_stations;
 }
 
+//allows the #of ship repair stations to be changed retroactively
 void warehouse::change_no_ship_repair_stations(int new_no_ship_repair_stations)
 {
 	no_ship_repair_stations = new_no_ship_repair_stations;
 }
 
+//returns #of truck loading bays
 int warehouse::get_no_truck_loading_bay()
 {
 	return no_truck_loading_bay;
 }
 
+//allows the #of truck loading bays to be changed retroactively
 void warehouse::change_no_truck_loading_bay(int new_no_truck_loading_bay)
 {
 	no_truck_loading_bay = new_no_truck_loading_bay;
 }
 
+//return #of ports
 int warehouse::get_no_ports()
 {
 	return no_ports;
 }
 
+//allows the #of ports to be changed retroactively
 void warehouse::change__no_ports(int new_no_ports)
 {
 	no_ports = new_no_ports;
 }
 
+//calculates the maintenance cost
 int warehouse::get_maintance_cost()
 {
 	return worker_capacity*5 + no_ship_repair_stations*4
